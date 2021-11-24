6645573ce077d144db8ea1b0dbf4ee1f3284aa69
zhuyulin <807715957@qq.com>
2021-11-24 11:42:36 +0800

    联调

--- src/components/Main/components/MobileMenu/MobileMenu.vue
+++ src/components/Main/components/MobileMenu/MobileMenu.vue
@@ -1,17 +1,32 @@
 <template>
   <div class="mobileMenuwrap">
     <!-- <el-cascader-panel :options="list" :props="propsOptions" /> -->
-    <div class="topTitle">福卡</div>
-    <div class="siderBar">
-    <van-sidebar v-model="sctive">
-      <van-sidebar-item v-for="(item,index) in list" :key="index" :title="item.name"  />
-    </van-sidebar>
-  </div>
-    <div class="centerList">
-        <div class="center" @click="getto" >
-          <div class="img"></div>
-          <span>查看报表</span>
+    <div v-for="item in list" :key="item.id">
+      <div class="topTitle">{{ item.name }}</div>
+    </div>
+    <div v-for="item in list" :key="item.id">
+      <div class="siderBar">
+        <van-sidebar v-model="sctive">
+          <van-sidebar-item
+            v-for="children in item.children"
+            :key="children.id"
+            :title="children.name"
+          />
+        </van-sidebar>
+      </div>
+      <div class="centerList">
+        <div v-for="childrens in item.children" :key="childrens.id">
+          <div
+            class="center"
+            @click="getto"
+            v-for="item in childrens.children"
+            :key="item.id"
+          >
+            <div class="img"></div>
+            <span>{{ item.name }}</span>
+          </div>
         </div>
+      </div>
     </div>
   </div>
 </template>
@@ -26,12 +41,12 @@ export default defineComponent({
   name: "SideMenu",
   components: {},
   props: {
-    list : Array as PropType<ColumnProps[]>,
+    list: Array as PropType<ColumnProps[]>,
   },
   setup(props) {
     const state = reactive({
       options: props,
-      sctive: 1,
+      sctive: 0,
     });
     return toRefs(state);
   },
@@ -40,10 +55,13 @@ export default defineComponent({
   },
   computed: {},
   methods: {
-    getto(){
-      console.log('跳转到新页面')
-      window.open('http://192.168.161.216:8088/superset/dashboard/world_health/?preselect_filters=%7B%7D&standalone=true&native_filters=%28%29','_blank') // 新窗口打开外链接
-    }
+    getto() {
+      console.log("跳转到新页面");
+      window.open(
+        "http://192.168.161.216:8088/superset/dashboard/world_health/?preselect_filters=%7B%7D&standalone=true&native_filters=%28%29",
+        "_blank"
+      ); // 新窗口打开外链接
+    },
   },
 });
 </script>
@@ -70,40 +88,40 @@ export default defineComponent({
       }
     }
   }
-  .centerList{
+  .centerList {
     float: left;
     margin-left: 10px;
     width: 200px;
     height: 100%;
-    .center{
+    .center {
       padding: 15px;
-      .img{
+      .img {
         width: 20px;
         height: 20px;
         background-color: #ccc;
         float: left;
       }
-      span{
+      span {
         margin-left: 10px;
       }
     }
   }
 }
-.van-sidebar{
+.van-sidebar {
   width: 100px !important;
-  .van-sidebar-item{
-  --van-sidebar-padding: 15px var(--van-padding-sm);
-}
-.van-sidebar-item--select::before {
-  height: 100% !important;
-  background-color:#1E90FF !important
-}
+  .van-sidebar-item {
+    --van-sidebar-padding: 15px var(--van-padding-sm);
+  }
+  .van-sidebar-item--select::before {
+    height: 100% !important;
+    background-color: #1e90ff !important;
+  }
 }
 
-.siderBar{
-    width: 100px !important;
-    height: 100%; 
-    float: left;
-    background-color: #f7f8fa;
+.siderBar {
+  width: 100px !important;
+  height: 100%;
+  float: left;
+  background-color: #f7f8fa;
 }
 </style>
