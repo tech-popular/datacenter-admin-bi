6db75e62a741c3ca256f6a66535cd42190b13b0f
张亚男 <zhangyanan>
2022-06-09 17:29:27 +0800

    update

--- src/api/api.ts
+++ src/api/api.ts
@@ -3,9 +3,11 @@ import { showMessage } from './status'
 import { ElMessage, ElLoading } from 'element-plus'
 import { IResponse, ILogin, ZLogin, IModelColumn, IModelSearch, VisitLog } from './type'
 // import base from './untils'
+let originHost = location.origin
 let axiosInstance: AxiosInstance = axios.create({
   // baseURL: base.baseurl,
-  baseURL: 'http://tech.9f.cn/canary-admin',
+  baseURL: originHost + '/canary-admin',
+  // baseURL: 'http://tech.9f.cn/canary-admin',
   // baseURL: 'http://192.168.161.219:8000/canary-admin',
   headers: {
     Accept: 'application/json',
