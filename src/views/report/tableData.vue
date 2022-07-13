9319d81c1f140c1377753da1d199e82c226d551e
张亚男 <zhangyanan>
2022-07-13 17:05:06 +0800

    时间条件查询

--- src/views/report/tableData.vue
+++ src/views/report/tableData.vue
@@ -5,7 +5,7 @@
         <el-form-item :label="searchForm.colBizName" prop="beginTimeValue">
           <el-date-picker v-model="searchForm.beginTimeValue" :clearable="false" type="date" placeholder="选择日期" class="demo-form-date"></el-date-picker>
         </el-form-item>
-        <el-form-item label="至" prop="endTimeValue" v-if="searchForm.endTimeValue" size="small">
+        <el-form-item label="至" prop="endTimeValue" v-if="conditiondataTextType.includes(searchForm.conditionType)" size="small">
           <el-date-picker v-model="searchForm.endTimeValue" :clearable="false" type="date" placeholder="选择日期" class="demo-form-date"></el-date-picker>
         </el-form-item>
       </div>
@@ -188,7 +188,8 @@ export default defineComponent({
         endTimeValue: '',
         colBizName: '',
         yearDateValue: '',
-        monthDateValue: ''
+        monthDateValue: '',
+        conditionType: 0
       }, // 查询条件报表数据
       dateParamVisible: true,
       dateOptionVisible: false,
@@ -605,18 +606,28 @@ export default defineComponent({
       this.handleCheckedFieldChange(this.checkedFields)
       // 统计日期
       this.conditionList = res.conditionList
-      if (res.conditionList.length) {
-        this.searchForm.colBizName = res.conditionList[0].colBizName
-      }
+      // if (res.conditionList.length) {
+      //   this.searchForm.colBizName = res.conditionList[0].colBizName
+      // }
       console.log('this.searchForm: ', this.searchForm)
       this.dateTextConditionHTMLList = res.dateTextConditionHTMLList
       this.dateOptionConditioHTMLList = ref.dateOptionConditioHTMLList
       if (res.dateTextConditionHTMLList.length) {
+        let beginTimeKeyData = JSON.parse(
+          res.dateTextConditionHTMLList[0].beginTimeKey
+        )
+        console.log('beginTimeKeyData: ', beginTimeKeyData)
+        this.searchForm.colBizName = beginTimeKeyData.colBizName
+        this.searchForm.conditionType = beginTimeKeyData.conditionType
         this.searchForm.beginTimeValue =
           res.dateTextConditionHTMLList[0].beginTimeValue
         if (res.dateTextConditionHTMLList[0].endTimeValue) {
           this.searchForm.endTimeValue =
             res.dateTextConditionHTMLList[0].endTimeValue
+        }
+        if (beginTimeKeyData.conditionType === 8) {
+          this.searchForm.beginTimeValue = ''
+          this.searchForm.endTimeValue = ''
         }
         this.dateParamVisible = true
       } else {
@@ -625,9 +636,12 @@ export default defineComponent({
       // 下拉年月日期条件
       if (res.dateOptionConditioHTMLList.length) {
         this.searchForm.yearDateValue =
-          res.dateTextConditionHTMLList[0].yearDateValue
+          res.dateOptionConditioHTMLList[0].yearDateValue
         this.searchForm.monthDateValue =
-          res.dateTextConditionHTMLList[0].monthDateValue
+          res.dateOptionConditioHTMLList[0].monthDateValue
+        this.searchForm.colBizName = JSON.parse(
+          res.dateOptionConditioHTMLList[0].yearDateKey
+        ).colBizName
         this.dateOptionVisible = true
       } else {
         this.dateOptionVisible = false
@@ -769,75 +783,6 @@ export default defineComponent({
       this.orderParams.splice(index, 1)
       this.orderParams.splice(addIndex, 0, item)
     }
-  },
-  // 自适应表格列宽
-  flexColumnWidth(str, tableData, flag = 'max') {
-    console.log('tableData: ', tableData)
-    console.log('str: ', str)
-    // str为该列的字段名(传字符串);tableData为该表格的数据源(传变量);
-    // flag为可选值，可不传该参数,传参时可选'max'或'equal',默认为'max'
-    // flag为'max'则设置列宽适配该列中最长的内容,flag为'equal'则设置列宽适配该列中第一行内容的长度。
-    str = str + ''
-    let columnContent = ''
-    if (
-      !tableData ||
-      !tableData.length ||
-      tableData.length === 0 ||
-      tableData === undefined
-    ) {
-      return
-    }
-    if (!str || !str.length || str.length === 0 || str === undefined) {
-      return
-    }
-    if (flag === 'equal') {
-      // 获取该列中第一个不为空的数据(内容)
-      for (let i = 0; i < tableData.length; i++) {
-        if (tableData[i][str].length > 0) {
-          // console.log('该列数据[0]:', tableData[0][str])
-          columnContent = tableData[i][str]
-          break
-        }
-      }
-    } else {
-      // 获取该列中最长的数据(内容)
-      let index = 0
-      for (let i = 0; i < tableData.length; i++) {
-        if (tableData[i][str] === null) {
-          return
-        }
-        const now_temp = tableData[i][str] + ''
-        const max_temp = tableData[index][str] + ''
-        if (now_temp.length > max_temp.length) {
-          index = i
-        }
-      }
-      columnContent = tableData[index][str]
-    }
-    // console.log('该列数据[i]:', columnContent)
-    // 以下分配的单位长度可根据实际需求进行调整
-    let flexWidth = 0
-    for (const char of columnContent) {
-      if ((char >= 'A' && char <= 'Z') || (char >= 'a' && char <= 'z')) {
-        // 如果是英文字符，为字符分配8个单位宽度
-        flexWidth += 8
-      } else if (char >= '\u4e00' && char <= '\u9fa5') {
-        // 如果是中文字符，为字符分配15个单位宽度
-        flexWidth += 15
-      } else {
-        // 其他种类字符，为字符分配8个单位宽度
-        flexWidth += 8
-      }
-    }
-    if (flexWidth < 80) {
-      // 设置最小宽度
-      flexWidth = 80
-    }
-    // if (flexWidth > 250) {
-    //   // 设置最大宽度
-    //   flexWidth = 250
-    // }
-    return flexWidth + 'px'
   }
 })
 </script>
