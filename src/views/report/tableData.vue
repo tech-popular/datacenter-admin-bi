8cf31d37ecc904fe909e5cd60789e5e024d96b5f
dingning <dingning@9fbank.com.cn>
2025-09-10 14:51:09 +0800

    注释导出按钮

--- src/views/report/tableData.vue
+++ src/views/report/tableData.vue
@@ -95,7 +95,7 @@
       </el-form-item>
       <el-form-item>
         <el-button type="primary" plain @click="onSearch">查询</el-button>
-        <el-button type="primary" plain @click="downLoad">导出</el-button>
+<!--        <el-button type="primary" plain @click="downLoad">导出</el-button>-->
       </el-form-item>
     </el-form>
     <el-table
